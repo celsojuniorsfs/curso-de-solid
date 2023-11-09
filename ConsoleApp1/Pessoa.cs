b15be255e8db6b5ee628df37718cf7156487525b
Celso Junior <celso.junior@visionnaire.com.br>
2023-11-09 19:23:10 -0300

    Curso de SOLID - Aula: Classes e Objetos

---
+++ ConsoleApp1/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula_Classes1
+{
+    class Pessoa
+    {
+        //field
+        public string nome;
+        public int idade;
+        public string genero;
+
+        public void Identificar()
+        {
+            Console.WriteLine($"Olá, sou o {nome} tenho {idade} e sou do sexo {genero}");
+        }
+    }
+
+
+}
+
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula_Classes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.nome = "Paulo";
+            pessoa1.idade = 45;
+            pessoa1.genero = "Masculino";
+            pessoa1.Identificar();
+        }
+    }
+}
+
+
